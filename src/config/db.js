ef3a1b86c6fa731b2a5e5e5fd55a54e26ab15200
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-01-03 18:09:48 +0700

    Attempt 2

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,6 @@ const Pool = require('pg').Pool;
 // dotenv package cho phép dùng biến môi trường được khai báo trong .env
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  database: process.env.PG_DATABASE,
-});
+const pool = new Pool();
 
 module.exports = pool;
