619503f161daf8a7312ea9c1a98debba056753bd
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-01-03 17:32:02 +0700

    Comment exam script

--- src/scripts/trigger.sql
+++ src/scripts/trigger.sql
@@ -20,7 +20,6 @@ BEGIN
 	RAISE NOTICE 'Updated total lesson in unit, chapter and course!';
 	RETURN NEW;
 END;
-
 $$
 LANGUAGE 'plpgsql'; 
 
@@ -364,41 +363,41 @@ CREATE OR REPLACE TRIGGER update_participants_course
 	FOR EACH ROW
 	EXECUTE PROCEDURE fn_increase_participants_course();
 -- Trigger auto increase total_exam after insert exam
-CREATE OR REPLACE FUNCTION increase_total_exam()
-	RETURNS trigger AS
-$$
-BEGIN
-	UPDATE exam_series SET total_exam = total_exam + 1 WHERE NEW.exam_series_id = exam_series_id;
+-- CREATE OR REPLACE FUNCTION increase_total_exam()
+-- 	RETURNS trigger AS
+-- $$
+-- BEGIN
+-- 	UPDATE exam_series SET total_exam = total_exam + 1 WHERE NEW.exam_series_id = exam_series_id;
 
-	RAISE NOTICE 'Auto increase total_exam successfully';
-	RETURN NEW;
-END;
-$$
-LANGUAGE 'plpgsql';
+-- 	RAISE NOTICE 'Auto increase total_exam successfully';
+-- 	RETURN NEW;
+-- END;
+-- $$
+-- LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE TRIGGER create_new_exam
-	AFTER INSERT ON exam
-	FOR EACH ROW
-	EXECUTE PROCEDURE increase_total_exam()  
+-- CREATE OR REPLACE TRIGGER create_new_exam
+-- 	AFTER INSERT ON exam
+-- 	FOR EACH ROW
+-- 	EXECUTE PROCEDURE increase_total_exam()  
 
 
---Trigger auto decrease total_lesson after delete exam
-CREATE OR REPLACE FUNCTION decrease_total_exam()
-	RETURNS trigger AS
-$$
-BEGIN
-	UPDATE exam_series SET total_exam = total_exam - 1 WHERE OLD.exam_series_id = exam_series_id;
+-- --Trigger auto decrease total_lesson after delete exam
+-- CREATE OR REPLACE FUNCTION decrease_total_exam()
+-- 	RETURNS trigger AS
+-- $$
+-- BEGIN
+-- 	UPDATE exam_series SET total_exam = total_exam - 1 WHERE OLD.exam_series_id = exam_series_id;
 
-	RAISE NOTICE 'Auto decrease total_exam successfully';
-	RETURN NEW;
-END;
-$$
-LANGUAGE 'plpgsql';
+-- 	RAISE NOTICE 'Auto decrease total_exam successfully';
+-- 	RETURN NEW;
+-- END;
+-- $$
+-- LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE TRIGGER delete_exam
-	AFTER DELETE ON exam
-	FOR EACH ROW
-	EXECUTE PROCEDURE decrease_total_exam()
+-- CREATE OR REPLACE TRIGGER delete_exam
+-- 	AFTER DELETE ON exam
+-- 	FOR EACH ROW
+-- 	EXECUTE PROCEDURE decrease_total_exam()
 
 
 
