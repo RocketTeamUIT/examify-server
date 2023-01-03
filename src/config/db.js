cd437623c55432b90453462ec4780f5d363931d6
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-01-03 19:18:39 +0700

    Connect db bug fixes

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,12 @@ const Pool = require('pg').Pool;
 // dotenv package cho phép dùng biến môi trường được khai báo trong .env
 require('dotenv').config();
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  database: process.env.PG_DATABASE,
+});
 
 module.exports = pool;
