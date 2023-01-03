83f1236aad5ab1988f09658291c22a1268ee5dca
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-01-03 18:25:16 +0700

    Attempt 3

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
   host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   dialect: 'postgres',
   logging: false,
 });
