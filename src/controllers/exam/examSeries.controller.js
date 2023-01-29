4d5d8e79a3fc5dba0f4054c264a24145269b14a5
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-01-29 14:15:54 +0700

    add some API CRU examSeries
    - API delete examSeries is being progress

--- src/controllers/exam/examSeries.controller.js
+++ src/controllers/exam/examSeries.controller.js
@@ -1,1 +1,94 @@
+const db = require('../../models/index');
 
+module.exports = {
+  getAllExamSeries: async (req, res, next) => {
+    try {
+      const listSeriesData = await db.ExamSeries.findAll();
+
+      res.status(200).json({
+        status: 200,
+        data: listSeriesData,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  getExamSeries: async (req, res, next) => {
+    try {
+      const examSeriesId = req?.params?.id;
+
+      const examSeriesData = await db.ExamSeries.findOne({
+        where: {
+          id: examSeriesId,
+        },
+      });
+
+      res.status(200).json({
+        status: 200,
+        data: examSeriesData,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  createExamSeries: async (req, res, next) => {
+    try {
+      const { name, author, createBy } = req.body;
+
+      const newSeries = await db.ExamSeries.create({
+        name,
+        author,
+        createBy,
+      });
+
+      res.status(200).json({
+        status: 200,
+        message: 'created an exam series success!',
+        data: newSeries,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  updateExamSeries: async (req, res, next) => {
+    try {
+      const examSeriesId = req?.params?.id;
+      const { name, author } = req.body;
+
+      await db.ExamSeries.update(
+        {
+          name,
+          author,
+        },
+        {
+          id: examSeriesId,
+        },
+      );
+
+      res.status(200).json({
+        status: 200,
+        message: 'updated an exam series success!',
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  deleteExamSeries: async (req, res, next) => {
+    try {
+      const examSeriesId = req?.params?.id;
+
+      // being processed...
+
+      res.status(200).json({
+        status: 200,
+        message: 'deleted an exam series success!',
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- src/routes/exam/examSeries.route.js
+++ src/routes/exam/examSeries.route.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const examSeriesController = require('../../controllers/exam/examSeries.controller');
+
+// [GET] /examSeries/ -> get all examSeries
+router.get('/', examSeriesController.getAllExamSeries);
+
+// [GET] /examSeries/:id -> get a examSeries
+router.get('/:id', examSeriesController.getExamSeries);
+
+// [GET] /examSeries/create/:id -> create a examSeries
+router.post('/create', examSeriesController.createExamSeries);
+
+// [GET] /examSeries/update/:id -> update a examSeries
+router.put('/update/:id', examSeriesController.updateExamSeries);
+
+// [GET] /examSeries/delete/:id -> delete a examSeries
+router.delete('/delete/:id', examSeriesController.deleteExamSeries);
+
 module.exports = router;
