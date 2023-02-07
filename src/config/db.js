497cd0bba51a39e0d0df1576e636875dbed227b2
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-02-07 23:44:25 +0700

    Change to connection string

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,8 @@
-const Pool = require("pg").Pool;
+const Pool = require('pg').Pool;
 
 // dotenv package cho phép dùng biến môi trường được khai báo trong .env
-require("dotenv").config();
+require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  database: process.env.PG_DATABASE,
-});
+const pool = new Pool(process.env.CONNECTION_STRING);
 
 module.exports = pool;
