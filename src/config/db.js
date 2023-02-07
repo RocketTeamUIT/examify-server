cf717ab86adc3a38bb73ca06763ed0ada1dbc494
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-02-07 23:56:37 +0700

    update

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,8 @@ const Pool = require('pg').Pool;
 // dotenv package cho phép dùng biến môi trường được khai báo trong .env
 require('dotenv').config();
 
-const pool = new Pool(process.env.CONNECTION_STRING);
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+});
 
 module.exports = pool;
