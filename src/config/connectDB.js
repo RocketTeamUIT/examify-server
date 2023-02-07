0799f6c04e60e5335a97e633fa3e9bac01cc5e8c
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2023-02-07 23:45:34 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,11 +3,7 @@ const { Sequelize } = require('sequelize');
 // dotenv package cho phép dùng biến môi trường được khai báo trong .env
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
-  host: process.env.PG_HOST,
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(process.env.CONNECTION_STRING);
 
 let connectDB = async () => {
   try {
